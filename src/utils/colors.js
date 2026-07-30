dbd505af1d49938c481cb4f2aca2fe4aae4b75bd
yunap <yunay@yahoo.com>
2026-07-30 03:20:06 -0700

    Redesign wardrobe color controls

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,9 +1,33 @@
 import {
   COLOR_TAXONOMY,
+  colorTaxonomyEntry,
   normalizeColorName,
 } from '../../lib/colorTaxonomy.js'
 
 export const COLOR_OPTIONS = COLOR_TAXONOMY
+export const COLOR_FAMILY_ORDER = [
+  'black', 'white', 'grey', 'beige', 'brown',
+  'red', 'pink', 'purple', 'blue', 'cyan', 'green',
+  'yellow', 'orange', 'metallic', 'multi',
+]
+
+export const COLOR_FAMILY_LABELS = Object.freeze({
+  black: 'Black',
+  white: 'White & cream',
+  grey: 'Grey',
+  beige: 'Beige & tan',
+  brown: 'Brown',
+  red: 'Red',
+  pink: 'Pink',
+  purple: 'Purple',
+  blue: 'Blue',
+  cyan: 'Teal & turquoise',
+  green: 'Green',
+  yellow: 'Yellow',
+  orange: 'Orange',
+  metallic: 'Metallic',
+  multi: 'Multi',
+})
 
 export const COLOR_HEX_MAP = {
   ...Object.fromEntries(COLOR_OPTIONS.map(color => [color.name, color.hex])),
@@ -18,7 +42,18 @@ export const LIGHT_COLORS = [
   'yellow', 'sage',
 ]
 
-export { normalizeColorName }
+export { normalizeColorName, colorTaxonomyEntry }
+
+export function colorsByFamily(values = COLOR_OPTIONS.map(color => color.name)) {
+  const allowed = new Set(values.map(normalizeColorName))
+  return COLOR_FAMILY_ORDER
+    .map(family => ({
+      family,
+      label: COLOR_FAMILY_LABELS[family],
+      colors: COLOR_OPTIONS.filter(color => color.family === family && allowed.has(color.name)),
+    }))
+    .filter(group => group.colors.length > 0)
+}
 
 export function getColorSwatch(value, fallback = '#C8C5C0') {
   return COLOR_HEX_MAP[normalizeColorName(value)] || fallback
